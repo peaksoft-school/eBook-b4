6f4c0f479957f691354066416301eb3917a3c66d
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-04-07 15:53:07 +0600

    Create empty project

---
+++ src/main/java/kg/peaksoft/ebookb4/EBookB4Application.java
@@ -0,0 +1,14 @@
+package kg.peaksoft.ebookb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookB4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookB4Application.class, args);
+        System.out.println("Welcome colleagues, project name is eBook!");
+    }
+
+}
---
+++ src/test/java/kg/peaksoft/ebookb4/EBookB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.ebookb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookB4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
