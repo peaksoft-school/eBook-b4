8a8c430a48217686c00f2629f12bb0b2b55e84e8
mac <shattyk7979@mail.ru>
2022-04-11 12:12:08 +0600

    Shattyk initial commit1

---
+++ src/main/java/kg/peaksoft/ebookb4/entities/Admin.java
@@ -0,0 +1,14 @@
+package kg.peaksoft.ebookb4.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long adminId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
