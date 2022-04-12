601fd46be8ab2de9dc97cf4d49b765da9a1217ea
mac <shattyk7979@mail.ru>
2022-04-12 18:44:10 +0600

    1

--- src/main/java/kg/peaksoft/ebookb4/service/UserService.java
+++ src/main/java/kg/peaksoft/ebookb4/service/UserService.java
@@ -5,7 +5,19 @@ import kg.peaksoft.ebookb4.entities.User;
 import java.util.List;
 import java.util.Optional;
 
-public class UserService {
+public interface UserService {
+    User saveUser(User user);
+
+    void removeUserById(Long id);
+
+    Optional<User> getById(Long id);
+
+
+    List<User> getAllUser();
+
+    User update(User user,Long id);
+
+    User findById(Long id);
 
 
 }
--- src/main/java/kg/peaksoft/ebookb4/service/UserServiceImpl.java
+++ src/main/java/kg/peaksoft/ebookb4/service/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package kg.peaksoft.ebookb4.service;
 
-public interface UserServiceImpl {
+import kg.peaksoft.ebookb4.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void removeUserById(Long id) {
+
+    }
+
+    @Override
+    public Optional<User> getById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public User update(User user, Long id) {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
 }
