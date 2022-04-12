ed7f7aca3361adb784af319d7ffebd257a02c1cc
mac <shattyk7979@mail.ru>
2022-04-13 02:19:10 +0600

    1

--- src/main/java/kg/peaksoft/ebookb4/enums/Role.java
+++ src/main/java/kg/peaksoft/ebookb4/enums/Role.java
@@ -1,4 +1,28 @@
 package kg.peaksoft.ebookb4.enums;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
-public enum Role {
-}
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter
+@Component
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
