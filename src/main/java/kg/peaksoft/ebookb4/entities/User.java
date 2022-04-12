0c2b2bfd15461777a5b33418092ce502e4ecf7cb
mac <shattyk7979@mail.ru>
2022-04-12 18:34:13 +0600

    new commit

--- src/main/java/kg/peaksoft/ebookb4/entities/User.java
+++ src/main/java/kg/peaksoft/ebookb4/entities/User.java
@@ -1,11 +1,32 @@
 package kg.peaksoft.ebookb4.entities;
 
-import javax.persistence.Entity;
-
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "gen")
+    @SequenceGenerator(
+            name = "gen",
+            sequenceName = "seq",
+            allocationSize = 1)
+
+    private Long clientId;
+    private String fullName;
+    private Role role;
+    private String email;
+    private String address;
+    private  int phoneNumber;
+
+    @Transient
+    private ApplicationUser applicationUser;
+
+    @OneToMany
+    private List<Promocode> promocode;
+
 
 }
