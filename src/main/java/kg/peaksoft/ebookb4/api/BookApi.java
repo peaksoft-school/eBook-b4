6117d3596ee25b782bde51f5b3c4c603d365bdaa
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-04-15 15:06:23 +0600

    changes BookApi

--- src/main/java/kg/peaksoft/ebookb4/api/BookApi.java
+++ src/main/java/kg/peaksoft/ebookb4/api/BookApi.java
@@ -8,42 +8,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/api/book")
+@RequestMapping("/api/books")
 @AllArgsConstructor
+@RolesAllowed("ROLE_VENDOR")
 public class BookApi {
 
     private final BookService bookService;
 
-    @PostMapping("/save/{userId}")
-    @PreAuthorize("hasRole('ROLE_VENDOR')")
+    @PostMapping("{userId}")
     public ResponseEntity<?> saveBook(@RequestBody BookRequest bookRequest,
                                       @PathVariable("userId") Long userId){
         return  bookService.register(bookRequest, userId);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
-    @GetMapping("/getById/{id}")
+    @GetMapping("{id}")
     public Book findById(@PathVariable Long id){
         return bookService.findByBookId(id);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
-    @GetMapping("/getAll")
+
+    @GetMapping
     public List<Book> findAll(){
         return bookService.findAll();
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
-    @DeleteMapping("/delete/{bookId}")
+    @DeleteMapping("{bookId}")
     public ResponseEntity<?> delete(@PathVariable Long bookId) {
         return bookService.delete(bookId);
     }
 
-    @PreAuthorize("hasAnyAuthority('ROLE_VENDOR')")
-    @PatchMapping("/update/{bookId}")
+    @PatchMapping("{bookId}")
     public ResponseEntity<?> update(@RequestBody BookRequest request,
                                     @PathVariable Long bookId){
         return bookService.update(request,bookId);
