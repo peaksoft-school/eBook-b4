d80f5234cf3471de0b18e19d4acb274afdfebaa3
VivoBook <kutu>
2022-04-21 12:27:15 +0600

    kutubek2

--- src/main/java/kg/peaksoft/ebookb4/api/VendorApi.java
+++ src/main/java/kg/peaksoft/ebookb4/api/VendorApi.java
@@ -2,6 +2,8 @@ package kg.peaksoft.ebookb4.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kg.peaksoft.ebookb4.db.models.bookClasses.Book;
+import kg.peaksoft.ebookb4.db.service.BookService;
 import kg.peaksoft.ebookb4.db.service.PromoService;
 import kg.peaksoft.ebookb4.dto.request.PromoRequest;
 import lombok.AllArgsConstructor;
@@ -9,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -18,14 +22,21 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Vendor",description = "Vendor accessible apis")
 public class VendorApi {
 
+    private BookService bookService;
     private PromoService promoService;
-
 
     @Operation(summary = "promo", description = "Vendor creates a promo")
     @PostMapping("/create-promo")
     public ResponseEntity<?> createPromo(@RequestBody PromoRequest promoRequest){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return promoService.createPromo(promoRequest,username);
+    }
+
+    @Operation(summary = "Vendor books", description = "Find all vendor books")
+    @GetMapping("/books")
+    public List<Book> findAllBookFromVendor() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return bookService.findBooksFromVendor(username);
     }
 
 }
--- src/main/java/kg/peaksoft/ebookb4/db/service/impl/BookGetServiceImpl.java
+++ src/main/java/kg/peaksoft/ebookb4/db/service/impl/BookGetServiceImpl.java
@@ -23,7 +23,6 @@ public class BookGetServiceImpl implements BookGetService {
     private final BookRepository repository;
     private PromoService promoService;
 
-
     @Override
     public List<Book> findByGenre(Genre genre) {
         promoService.checkPromos();
