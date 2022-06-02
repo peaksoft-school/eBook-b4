3f0e2c7409b59166c1b9ffae22542d97a6355201
VivoBook <kutu>
2022-06-02 13:49:32 +0600

    feature vendor Count books and delete profile 3

--- src/main/java/kg/peaksoft/ebookb4/config/services/UserDetailsImpl.java
+++ src/main/java/kg/peaksoft/ebookb4/config/services/UserDetailsImpl.java
@@ -11,58 +11,59 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collections;
 import java.util.List;
 
-@Getter @Setter
+@Getter
+@Setter
 public class UserDetailsImpl implements UserDetails {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  private Long id;
+    private Long id;
 
-  private String email;
+    private String email;
 
-  @JsonIgnore
-  private String password;
+    @JsonIgnore
+    private String password;
 
-  private String number;
+    private String number;
 
-  private String firstName;
+    private String firstName;
 
-  private String lastName;
+    private String lastName;
 
-  private List<? extends GrantedAuthority> authorities;
+    private List<? extends GrantedAuthority> authorities;
 
-  private boolean isAccountNonExpired = true;
-  private boolean isAccountNonLocked = true;
-  private boolean isCredentialsNonExpired = true;
-  private boolean isEnabled = true;
+    private boolean isAccountNonExpired = true;
+    private boolean isAccountNonLocked = true;
+    private boolean isCredentialsNonExpired = true;
+    private boolean isEnabled = true;
 
-  public UserDetailsImpl(Long id, String email, String password,
-                         List<? extends GrantedAuthority> authorities, String number,
-                         String firstName, String lastName) {
-    this.id = id;
-    this.email = email;
-    this.password = password;
-    this.authorities = authorities;
-    this.number = number;
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+    public UserDetailsImpl(Long id, String email, String password,
+                           List<? extends GrantedAuthority> authorities, String number,
+                           String firstName, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-  public static UserDetailsImpl build(User user) {
-    SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName().name());
+    public static UserDetailsImpl build(User user) {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName().name());
 
-    return new UserDetailsImpl(
-        user.getId(), 
-        user.getEmail(),
-        user.getPassword(),
-            Collections.singletonList(authority), user.getNumber(),
-            user.getFirstName(),
-            user.getLastName()
-            );
-  }
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(authority), user.getNumber(),
+                user.getFirstName(),
+                user.getLastName()
+        );
+    }
 
 
-  @Override
-  public String getUsername() {
-    return email;
-  }
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
