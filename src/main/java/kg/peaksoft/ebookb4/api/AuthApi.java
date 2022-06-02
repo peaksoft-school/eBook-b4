4411e5bd707bd5fa166562f7bf43fed4c85e0828
VivoBook <kutu>
2022-06-02 14:44:24 +0600

    delete import mail sendler

--- src/main/java/kg/peaksoft/ebookb4/api/AuthApi.java
+++ src/main/java/kg/peaksoft/ebookb4/api/AuthApi.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.mail.MessagingException;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
