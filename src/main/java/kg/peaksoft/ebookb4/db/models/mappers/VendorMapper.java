b7cf0d7fe053ddb30b1e3597602f16f4439a8cf1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 11:32:08 +0600

    Refactor VendorMapper

--- src/main/java/kg/peaksoft/ebookb4/db/models/mappers/VendorMapper.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/mappers/VendorMapper.java
@@ -1,6 +1,5 @@
 package kg.peaksoft.ebookb4.db.models.mappers;
 
-
 import kg.peaksoft.ebookb4.db.models.entity.User;
 import kg.peaksoft.ebookb4.db.models.response.VendorResponse;
 import org.springframework.stereotype.Component;
@@ -8,16 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class VendorMapper {
 
-
-
     public VendorResponse createVendorDto(User user) {
         VendorResponse vendorResponse = new VendorResponse();
         vendorResponse.setVendorId(user.getId());
         vendorResponse.setFirstName(user.getFirstName());
         vendorResponse.setLastName(user.getLastName());
-
-//        vendorResponse.setLastName(user.getLastName()+" "+user.getFirstName());
-
         vendorResponse.setPhoneNumber(user.getNumber());
         vendorResponse.setEmail(user.getEmail());
         vendorResponse.setAmountOfBooks(user.getVendorAddedBooks().size());
