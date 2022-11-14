c96e5922a488f9494ab4f60de82f3b2e42919634
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 10:43:34 +0600

    Refactor Basket entity

--- src/main/java/kg/peaksoft/ebookb4/db/models/booksClasses/Basket.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/booksClasses/Basket.java
@@ -3,16 +3,26 @@ package kg.peaksoft.ebookb4.db.models.booksClasses;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import kg.peaksoft.ebookb4.db.models.entity.Book;
 import kg.peaksoft.ebookb4.db.models.entity.User;
-import lombok.*;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Entity
 public class Basket {
 
     @Id
@@ -36,4 +46,5 @@ public class Basket {
     public void clear() {
         books.removeIf(books -> true);
     }
+
 }
