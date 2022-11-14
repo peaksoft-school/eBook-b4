fca2dd628d236b272dca3a928727811c1d841211
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 11:07:13 +0600

    Refactor ClientUpdateDTO

--- src/main/java/kg/peaksoft/ebookb4/db/models/dto/ClientUpdateDTO.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/dto/ClientUpdateDTO.java
@@ -6,22 +6,22 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 24/4/22
- */
 @Getter
 @Setter
 public class ClientUpdateDTO {
 
     private String firstName;
+
     @NotBlank
     @Size(min = 6, max = 40)
     private String oldPassword;
+
     @NotBlank
     @Size(min = 6, max = 40)
     private String newPassword;
+
     @NotBlank
     @Size(min = 6, max = 40)
     private String confirmNewPassword;
+
 }
