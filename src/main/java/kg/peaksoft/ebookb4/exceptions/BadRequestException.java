d1f646a39de8b453f315484bd7faaf71fb77caac
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 15:06:03 +0600

    Refactor BadRequestException

--- src/main/java/kg/peaksoft/ebookb4/exceptions/BadRequestException.java
+++ src/main/java/kg/peaksoft/ebookb4/exceptions/BadRequestException.java
@@ -3,15 +3,11 @@ package kg.peaksoft.ebookb4.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.function.Supplier;
-
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 12/4/22
- */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
+
     public BadRequestException(String message) {
         super(message);
     }
+
 }
