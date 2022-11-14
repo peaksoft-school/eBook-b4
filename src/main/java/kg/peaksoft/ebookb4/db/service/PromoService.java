6727590dfbb6b8e84719a80c8c0910e7145bf6c8
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 14:42:27 +0600

    Refactor PromoService

--- src/main/java/kg/peaksoft/ebookb4/db/service/PromoService.java
+++ src/main/java/kg/peaksoft/ebookb4/db/service/PromoService.java
@@ -3,10 +3,6 @@ package kg.peaksoft.ebookb4.db.service;
 import kg.peaksoft.ebookb4.dto.request.PromoRequest;
 import org.springframework.http.ResponseEntity;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 19/4/22
- */
 public interface PromoService {
 
     ResponseEntity<?> createPromo(PromoRequest promoRequest, String username);
