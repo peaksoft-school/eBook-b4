e6140bc4937c89b8f316e11254314f6cc197b3c3
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 14:10:22 +0600

    Refactor ClientOperationRepository

--- src/main/java/kg/peaksoft/ebookb4/db/repository/ClientOperationRepository.java
+++ src/main/java/kg/peaksoft/ebookb4/db/repository/ClientOperationRepository.java
@@ -2,6 +2,9 @@ package kg.peaksoft.ebookb4.db.repository;
 
 import kg.peaksoft.ebookb4.db.models.booksClasses.ClientOperations;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientOperationRepository extends JpaRepository<ClientOperations, Long> {
+
 }
