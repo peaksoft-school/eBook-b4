75d5f6393f813a7c6338caf38d446de4d978a8f0
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 13:45:09 +0600

    Refactor CardResponse

--- src/main/java/kg/peaksoft/ebookb4/db/models/response/CardResponse.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/response/CardResponse.java
@@ -5,26 +5,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CardResponse {
+
     private Long bookId;
+
     private String title;
+
     private String authorFullName;
+
     private String aboutBook;
+
     private String publishingHouse;
+
     private int yearOfIssue;
+
     private Double price;
+
     private Double sumAfterDiscount;
+
     private Integer promoDiscount;
+
     int countOfBooksInTotal;
+
     Integer discount;
-//    Double sum;
-//    Double total;
 
     int countOfPaperBook = 0;
 
