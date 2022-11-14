df06f4e251334dd0eab0451bef561bbda1b7a7d4
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 13:48:24 +0600

    Refactor VendorResponse

--- src/main/java/kg/peaksoft/ebookb4/db/models/response/VendorResponse.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/response/VendorResponse.java
@@ -1,24 +1,26 @@
 package kg.peaksoft.ebookb4.db.models.response;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
-
 
 @Getter
 @Setter
 public class VendorResponse {
 
     private Long vendorId;
+
     private String firstName;
-    private String lastName ;
+
+    private String lastName;
+
     private String phoneNumber;
+
     private String email;
+
     private int amountOfBooks;
+
     private LocalDate dateOfRegistration;
 
-    public void gets(String firstName,String lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 }
