05217a8641e1bf8e5ee8776d3b24d041f5d0cb1e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 13:38:07 +0600

    Refactor RefuseBookRequest

--- src/main/java/kg/peaksoft/ebookb4/db/models/request/RefuseBookRequest.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/request/RefuseBookRequest.java
@@ -3,10 +3,6 @@ package kg.peaksoft.ebookb4.db.models.request;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 25/4/22
- */
 @Getter
 @Setter
 public class RefuseBookRequest {
