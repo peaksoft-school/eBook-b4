3a65ab7ed950c54fa7cbbe8c70973559f2bb2bb4
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 15:07:32 +0600

    Refactor NotFoundException

--- src/main/java/kg/peaksoft/ebookb4/exceptions/NotFoundException.java
+++ src/main/java/kg/peaksoft/ebookb4/exceptions/NotFoundException.java
@@ -3,16 +3,11 @@ package kg.peaksoft.ebookb4.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 12/4/22
- */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends RuntimeException{
-    public NotFoundException() {
-    }
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
         super(message);
     }
+
 }
