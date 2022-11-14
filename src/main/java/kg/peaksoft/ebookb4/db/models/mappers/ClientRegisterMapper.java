66c04e6821df90be20940eddbc3be15150f6aa7a
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 11:31:16 +0600

    Refactor ClientRegisterMapper

--- src/main/java/kg/peaksoft/ebookb4/db/models/mappers/ClientRegisterMapper.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/mappers/ClientRegisterMapper.java
@@ -4,14 +4,10 @@ import kg.peaksoft.ebookb4.db.models.dto.ClientRegisterDTO;
 import kg.peaksoft.ebookb4.db.models.entity.User;
 import org.springframework.stereotype.Component;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 24/4/22
- */
 @Component
 public class ClientRegisterMapper {
 
-    public ClientRegisterDTO createDTO(User user){
+    public ClientRegisterDTO createDTO(User user) {
         ClientRegisterDTO dto = new ClientRegisterDTO();
         dto.setEmail(user.getEmail());
         dto.setFirstName(user.getFirstName());
