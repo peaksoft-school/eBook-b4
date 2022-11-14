eae89cc0108e485055b5d70b11a3301a3eaa3df1
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 10:52:09 +0600

    Refactor FileInformation entity

--- src/main/java/kg/peaksoft/ebookb4/db/models/booksClasses/FileInformation.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/booksClasses/FileInformation.java
@@ -5,27 +5,38 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Entity
 public class FileInformation {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "file_id")
     private Long fileId;
 
     private String firstPhoto;
+
     private String secondPhoto;
+
     private String thirdPhoto;
+
     private String bookFile;
 
     private String keyOfFirstPhoto;
+
     private String keyOfSecondPhoto;
+
     private String keyOfThirdPhoto;
+
     private String KeyOfBookFile;
 
 }
