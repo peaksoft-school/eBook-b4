570cef18ff579efce38b166f39a724a589a61f11
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 13:37:50 +0600

    Refactor PromoRequest

--- src/main/java/kg/peaksoft/ebookb4/db/models/request/PromoRequest.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/request/PromoRequest.java
@@ -5,17 +5,16 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
-/**
- * Author: Zhanarbek Abdurasulov
- * Date: 19/4/22
- */
 @Getter
 @Setter
 public class PromoRequest {
 
     private String promoName;
+
     private LocalDate beginningDay;
+
     private LocalDate endDay;
+
     private int discount;
 
 }
