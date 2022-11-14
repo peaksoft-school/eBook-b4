eff13ef3f4b0fe172c3869e71f0c2413d1a12ec6
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 14:12:12 +0600

    Refactor PlaceCountRepository

--- src/main/java/kg/peaksoft/ebookb4/db/repository/PlaceCountRepository.java
+++ src/main/java/kg/peaksoft/ebookb4/db/repository/PlaceCountRepository.java
@@ -2,6 +2,9 @@ package kg.peaksoft.ebookb4.db.repository;
 
 import kg.peaksoft.ebookb4.db.models.entity.PlaceCounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaceCountRepository extends JpaRepository<PlaceCounts, Long> {
+
 }
