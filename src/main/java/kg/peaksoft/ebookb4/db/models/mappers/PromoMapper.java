643ffbb6cc6da35ae7669c15e0110356269aa293
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-11-14 11:31:39 +0600

    Refactor PromoMapper

--- src/main/java/kg/peaksoft/ebookb4/db/models/mappers/PromoMapper.java
+++ src/main/java/kg/peaksoft/ebookb4/db/models/mappers/PromoMapper.java
@@ -4,18 +4,15 @@ import kg.peaksoft.ebookb4.db.models.request.PromoRequest;
 import kg.peaksoft.ebookb4.db.models.booksClasses.PromoCode;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class PromoMapper {
 
-    public PromoCode create(PromoRequest promoRequest){
+    public PromoCode create(PromoRequest promoRequest) {
         PromoCode promocode = new PromoCode();
-
         promocode.setPromoCode(promoRequest.getPromoName());
         promocode.setBeginningDay(promoRequest.getBeginningDay());
         promocode.setEndDay(promoRequest.getEndDay());
         promocode.setDiscount(promoRequest.getDiscount());
-
         return promocode;
     }
 
